05ca86670593cd1f4fba983e77c55f283fda1a28
Brian <brian.lira@sp.senai.br>
2018-04-05 16:54:10 -0300

    Fix /api/mentorado/{id}/aplicacoes [GET]

--- Eaton.Mentoria.WebApi/Controllers/MentoradoController.cs
+++ Eaton.Mentoria.WebApi/Controllers/MentoradoController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Eaton.Mentoria.WebApi.Controllers
 {
-   /// <summary>
+    /// <summary>
     /// O controller Mentorado é responsável por:
     /// Cadastrar Mentorados utilizando o verbo POST
     /// listar todos mentorados utilizando o verbo GET
@@ -25,7 +25,7 @@ namespace Eaton.Mentoria.WebApi.Controllers
         public MentoradoController(IAplicacaoRepository aplicacaoRepository)
         {
             this.aplicacaoRepository = aplicacaoRepository;
-        }      
+        }
 
         /// <summary>
         /// Retorna os mentorados no formato JSON
@@ -37,9 +37,43 @@ namespace Eaton.Mentoria.WebApi.Controllers
         [Route("{id}/aplicacoes")]
         public IActionResult GetAplicacoes([FromRoute]int id)
         {
-            var aplicacoes = aplicacaoRepository.
-                                                    Listar(new string[] { "Mentorado", "Mentorado.Perfil", "Mentoria" }).
-                                                    Where(x => x.MentoradoId == id);
+            var aplicacoes = aplicacaoRepository
+                .Listar(new string[] { "Mentorado", "Mentorado.Perfil", "Mentoria", "Mentoria.Categoria" })
+                .Where(x => x.MentoradoId == id)
+                .Select(obj => new
+                {
+                    id = obj.AplicacaoId,
+                    usuario = new
+                    {
+                        id = obj.Mentorado.UsuarioId,
+                        email = obj.Mentorado.Email,
+                        password = "",
+                        role = obj.Mentorado.Role,
+                        ativo = obj.Mentorado.Ativo,
+                        perfil = new
+                        {
+                            id = obj.Mentorado.UsuarioId,
+                            nome = obj.Mentorado.Perfil?.Nome,
+                            miniBio = obj.Mentorado.Perfil?.MiniBio,
+                            foto = obj.Mentorado.Perfil?.Foto,
+                            cep = obj.Mentorado.Perfil?.Cep,
+                            sedeId = obj.Mentorado.Perfil?.SedeId
+                        },
+                    },
+                    mentoria = new
+                    {
+                        id = obj.Mentoria.MentoriaId,
+                        nome = obj.Mentoria.Nome,
+                        categoria = new
+                        {
+                            nome = obj.Mentoria.Categoria.Nome,
+                            id = obj.Mentoria.Categoria.CategoriaId,
+                        }
+                    },
+                    formulario = obj.justificativa,
+                    aceite = obj.Aceite
+
+                });
 
             return Ok(aplicacoes);
         }
